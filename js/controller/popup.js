1209144b54eca705b6cd10e90d1e6d8a17e48cef
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-03 04:40:00 +0700

    fix

--- js/controller/popup.js
+++ js/controller/popup.js
@@ -1,25 +1,21 @@
-import {toLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
-import {toStringHDMS} from 'https://cdn.skypack.dev/ol/coordinate.js';
-import {overlay,map,popupinfo,idmarker} from '../config/configpeta.js';
-import {URLGeoJson, clickpopup, urlPostGCF} from '../template/template.js';
-import {insertMarker,deleteMarker} from './marker.js';
-import {setInner,textBlur,onClick, getValue,setValue} from 'https://jscroot.github.io/element/croot.js';
+import { toLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+import { toStringHDMS } from 'https://cdn.skypack.dev/ol/coordinate.js';
+import { overlay, map, popupinfo, idmarker } from '../config/configpeta.js';
+import { URLGeoJson, clickpopup, urlPostGCF } from '../template/template.js';
+import { insertMarker, deleteMarker } from './marker.js';
+import { setInner, textBlur, onClick, getValue, setValue } from 'https://jscroot.github.io/element/croot.js';
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import { setCookieWithExpireHour } from 'https://jscroot.github.io/cookie/croot.js';
+import { setCookieWithExpireHour, getCookie } from 'https://jscroot.github.io/cookie/croot.js';
 
 let userToken;
 
+// Function to get the token from cookies with a specific parameter
 export function getTokenFromCookies(cookieName) {
-  const cookies = document.cookie.split(';');
-  for (const cookie of cookies) {
-    const [name, value] = cookie.trim().split('=');
-    if (name === cookieName) {
-      userToken = value; // Store the token in a global variable
-      return value;
-    }
-  }
-  return null;
+  const token = getCookie(cookieName);
+  return token;
 }
+
+// Function to get the token from API and store it in cookies
 export function getTokenFromAPI() {
   const tokenUrl = "https://us-central1-gcpospasial.cloudfunctions.net/login-gis";
   fetch(tokenUrl)
@@ -34,18 +30,20 @@ export function getTokenFromAPI() {
     .catch(error => console.error('Gagal mengambil token:', error));
 }
 
-
+// Retrieve the token from cookies
+userToken = getTokenFromCookies('user_token');
 
 export function onClosePopupClick() {
-    overlay.setPosition(undefined);
-    textBlur('popup-closer');
+  overlay.setPosition(undefined);
+  textBlur('popup-closer');
 }
 
 export function onDeleteMarkerClick() {
-    let idmarker = getValue('idmarker');
-    popupinfo.setPosition(undefined);
-    deleteMarker(idmarker);
+  let idmarker = getValue('idmarker');
+  popupinfo.setPosition(undefined);
+  deleteMarker(idmarker);
 }
+
 export function onSubmitMarkerClick() {
   let long = getValue('long');
   let lat = getValue('lat');
@@ -68,29 +66,28 @@ export function onSubmitMarkerClick() {
   console.log(name);
 }
 
-
-function afterSubmitCOG(result){
-    console.log(result);
+function afterSubmitCOG(result) {
+  console.log(result);
 }
 
 function popupInputMarker(evt) {
-    let tile = evt.coordinate;
-    let coordinate = toLonLat(tile);
-    let msg = clickpopup.replace("#LONG#",coordinate[0]).replace("#LAT#",coordinate[1]).replace('#X#',tile[0]).replace('#Y#',tile[1]).replace('#HDMS#',toStringHDMS(coordinate));
-    msg = 'ID : '+idmarker.id+'<br>'+msg + "Pixel : "+evt.pixel+"<br>"
-    setInner('popup-content',msg);
-    setValue('long',coordinate[0]);
-    setValue('lat',coordinate[1]);
-    overlay.setPosition(tile);
+  let tile = evt.coordinate;
+  let coordinate = toLonLat(tile);
+  let msg = clickpopup.replace("#LONG#", coordinate[0]).replace("#LAT#", coordinate[1]).replace('#X#', tile[0]).replace('#Y#', tile[1]).replace('#HDMS#', toStringHDMS(coordinate));
+  msg = 'ID: ' + idmarker.id + '<br>' + msg + "Pixel: " + evt.pixel + "<br>"
+  setInner('popup-content', msg);
+  setValue('long', coordinate[0]);
+  setValue('lat', coordinate[1]);
+  overlay.setPosition(tile);
 }
 
-function popupGetMarker(evt,features) {
-    let title = features.get('name');
-    setInner('popupinfo-title',title);
-    setValue('idmarker',features.get('id'));
-    let ctnt = "type : "+features.getGeometry().getType()+"<br>XY : "+toLonLat(evt.coordinate);
-    setInner('popupinfo-content',ctnt);
-    popupinfo.setPosition(evt.coordinate);
+function popupGetMarker(evt, features) {
+  let title = features.get('name');
+  setInner('popupinfo-title', title);
+  setValue('idmarker', features.get('id'));
+  let ctnt = "type: " + features.getGeometry().getType() + "<br>XY: " + toLonLat(evt.coordinate);
+  setInner('popupinfo-content', ctnt);
+  popupinfo.setPosition(evt.coordinate);
 }
 
 export function onMapPointerMove(evt) {
@@ -100,6 +97,7 @@ export function onMapPointerMove(evt) {
 }
 
 let popover;
+
 export function disposePopover() {
   if (overlay || popupinfo) {
     overlay.setPosition(undefined);
@@ -110,23 +108,23 @@ export function disposePopover() {
 export function onMapClick(evt) {
   const typeSelect = document.getElementById('type');
   let val = typeSelect.value
-    let feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
-      return feature;
-    });
-    overlay.setPosition(undefined);
-    popupinfo.setPosition(undefined);
-    if (!feature) {
-        popupInputMarker(evt);
-        return;
-    }else if (val == "Polygon" || val == "LineString" || val == "Circle" || val == "Point"){
-      popupInputMarker(evt)
-    }else{
-      popupGetMarker(evt,feature);
-    }
+  let feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+    return feature;
+  });
+  overlay.setPosition(undefined);
+  popupinfo.setPosition(undefined);
+  if (!feature) {
+    popupInputMarker(evt);
+    return;
+  } else if (val == "Polygon" || val == "LineString" || val == "Circle" || val == "Point") {
+    popupInputMarker(evt)
+  } else {
+    popupGetMarker(evt, feature);
   }
+}
 
 export function GetLonLat(evt) {
-    var point = map.getCoordinateFromPixel(evt.pixel);
-    var lonLat = ol.proj.toLonLat(point); 
-    console.log(lonLat);  // note the ordering of the numbers
+  var point = map.getCoordinateFromPixel(evt.pixel);
+  var lonLat = ol.proj.toLonLat(point);
+  console.log(lonLat);  // note the ordering of the numbers
 }

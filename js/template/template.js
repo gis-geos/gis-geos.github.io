7b52c55316d9acdad936cfdad27f2da5b0f433be
harisriyoni3 <1214047@std.ulbi.ac.id>
2023-11-17 01:15:33 +0700

    fix

--- js/template/template.js
+++ js/template/template.js
@@ -2,6 +2,9 @@ export let URLGeoJson = "https://us-central1-gcpospasial.cloudfunctions.net/hllo
 export let urlPostGCF = "https://us-central1-gcpospasial.cloudfunctions.net/POSTGEO";
 export let tableTag="tr";
 export let tableRowClass="content is-small";
+export let URLPost = `https://us-central1-gcpospasial.cloudfunctions.net/RegisterGis`
+export let URLLogin = `https://us-central1-gcpospasial.cloudfunctions.net/login-gis`
+export let token = 'token';
 export let tableTemplate=`
 <td>#NAME#</td>
 <td >#KORDINAT#</td>
--- jscroot/js/template/template.js
+++
@@ -1,4 +0,0 @@
-
-export let URLPost = `https://us-central1-gcpospasial.cloudfunctions.net/RegisterGis`
-export let URLLogin = `https://us-central1-gcpospasial.cloudfunctions.net/login-gis`
-export let token = 'token';
